47c0f9b44fec9a92c39e33565c018f26db2ad2fe
tomschweers <trschweers@gmail.com>
2018-10-05 12:40:14 -0700

    homepage

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -17,18 +17,16 @@ class Home extends Component {
           </h1>
           <p>
                   I will be using this to collect and showcase my development work as I begin building my portfolio of
-                  projects. The site itself is the first piece of my development portfolio.
+                  projects. This site itself is the first piece of my development portfolio.
             <br />
             <br />
                   If you would like to make a similar site for yourself using
                   this site as a starting point, clone/download my repository
-                  &nbsp;
             <a href='https://github.com/tomschweers/react-portfolio'target='_blank'>here</a>.
             <br />
             <br />
                   If you are more interested in cloning a similar, but more basic, boilerplate for building a web application
                   that is also mobile compatible, clone/download my repository
-                  &nbsp;
             <a href='https://github.com/tomschweers/react-boilerplate'target='_blank'>here</a>.
           </p>
         </div>
