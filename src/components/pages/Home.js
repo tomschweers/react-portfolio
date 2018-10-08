079b87ef0cd00fcc9044ca46e17415af122a89ff
tomschweers <trschweers@gmail.com>
2018-10-08 13:27:04 -0700

    clean up

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -21,15 +21,11 @@ class Home extends Component {
             <br />
             <br />
                   If you would like to make a similar site for yourself using
-                  this site as a starting point, clone/download my repository
-                  &nbsp;
-            <a href='https://github.com/tomschweers/react-portfolio'target='_blank'>here</a>.
+                  this site as a starting point, clone/download my repository <a href='https://github.com/tomschweers/react-portfolio'target='_blank'>here</a>.
             <br />
             <br />
                   If you are more interested in cloning a similar, but more basic, boilerplate for building a web application
-                  that is also mobile compatible, clone/download my repository
-                  &nbsp;
-            <a href='https://github.com/tomschweers/react-boilerplate'target='_blank'>here</a>.
+                  that is also mobile compatible, clone/download my repository <a href='https://github.com/tomschweers/react-boilerplate'target='_blank'>here</a>.
           </p>
         </div>
       </div>
